99490cbdd07e58fc901f24d6ac377e0751a5055e
Angelina <angelinarwhite@gmail.com>
2022-04-13 14:14:10 -0400

    add content to aboutme.html

---
+++ script.js
@@ -0,0 +1,9 @@
+const intro = document.createElement("h4");
+intro.textContent = "Hi, I'm Angelina!";
+
+document.querySelector("body").appendChild(intro);
+
+const flat = document.createElement("h5");
+flat.textContent = "I am a software engineering student at Flatiron school learning fullstack development. This website is my project for the prework section of the class. My class will end at the beginning of August. At that time, I will be seeking full time employment as a web developer.";
+
+document.querySelector("body").appendChild(flat);
